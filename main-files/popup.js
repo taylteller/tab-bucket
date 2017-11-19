0319adf895fdbb3b637a15b54ebc7d93a6340909
Taylor Najjar <16545112+taylteller@users.noreply.github.com>
2017-11-18 22:08:09 -0500

    add functionality to display random saved page

--- main-files/popup.js
+++ main-files/popup.js
@@ -35,43 +35,53 @@ display it in new tab
 fix UI
 
 should also be a button to manage saved pages
-when clicked it opens a page with all the bookmarks and option to remove them
+when clicked it opens a page with all the saved pages and option to remove them
 could use a specific html page that opens in a new tab, a foreach that displays
 each array item as an openable link (in new tab) with a remove option next to it
+
+^could also have an option to import all your bookmarks, if that's feasible
+or if not, maybe just select randomly from bookmarks AND saved pages (as an option)
 */
 
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.sync.get({storedUrls: []}, function (result) {
-    console.log('result.storedUrls', result.storedUrls);
+
     var storedUrls = result.storedUrls;
     var saveOrRemove = document.getElementById('save-or-remove');
+    var noDisplay = document.getElementById('no-display');
+
     getCurrentTabUrl((url) => {
       var index = storedUrls.indexOf(url);
       if (index > -1) {
         saveOrRemove.innerText = "Remove from saved pages";
       }
-      console.log('index', index);
 
       //if user clicks save or remove, it should save or remove page as appropriate
       saveOrRemove.addEventListener('click', function () {
+        noDisplay.style.display = "none";
         if (index > -1) {
           //remove url from array, update button & index
           storedUrls.splice(index, 1);
           saveOrRemove.innerText = "Save current page";
           index = -1;
-          console.log('index2', index);
         } else {
           //add url to array, update button & index
           storedUrls.push(url);
           saveOrRemove.innerText = "Remove from saved pages";
           index = storedUrls.length - 1;
-          console.log('index3', index);
         }
         //then add it back to the stored array
         chrome.storage.sync.set({storedUrls: storedUrls});
-        chrome.storage.sync.get(null, function (result) {
-          console.log('result after setting', result);
-        });
+      });
+
+      display.addEventListener('click', function () {
+        if (storedUrls.length > 0) {
+          var randomUrl = storedUrls[Math.floor(Math.random() * storedUrls.length)];
+          console.log('randomUrl',randomUrl);
+          chrome.tabs.create({ url: randomUrl });
+        } else {
+          noDisplay.style.display = "block";
+        }
       });
     });
   });
